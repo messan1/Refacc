cece5bd29bc3a404c48e634ed217dd9c79aa1eb3
Messan christian <messanchristian@hotmail.com>
2019-07-28 17:20:56 +0000

    google trakink

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "UA-144706102-1",
+        trackingId: 'UA-144706102-1',
 
         head: false,
 
