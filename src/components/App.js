c52b609c48572ffcb26d6fdae42e4a03a9e44abf
Messan christian <messanchristian@hotmail.com>
2019-07-28 00:52:20 +0000

    Galerie now ready

--- src/components/App.js
+++ src/components/App.js
@@ -55,7 +55,7 @@ function App () {
         <Link
           className="text-lg lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-grefacc text-black font-bold"
           exact
-          to="/"
+          to="/la-galerie-des-jfac"
           activeClassName="active"
         >
           Galerie
--- src/components/photos.js
+++ src/components/photos.js
@@ -1,92 +1,107 @@
 export const photos = [
   {
-    src: "https://source.unsplash.com/2ShvY8Lf6l0/800x599",
+    src: "https://media.graphcms.com/XxUFUwgWTOWLnn205keH",
     width: 4,
     height: 3
   },
   {
-    src: "https://source.unsplash.com/Dm-qxdynoEc/800x799",
+    src: "https://media.graphcms.com/UHfdTZ4oQ92OUzangpB5",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://media.graphcms.com/xocAACtASa6Dsh2faU9w",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://media.graphcms.com/Gkqy0WArTtikUstaBlIQ",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://media.graphcms.com/QIJvZlOMTH6eHQKnS2SQ",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://media.graphcms.com/iOkbhRtPQueeX9ZQwvt5",
+    width: 4,
+    height: 3
+  },
+  {
+    src: "https://media.graphcms.com/2WSNK0TSHqxdMKQcDp8A",
     width: 1,
     height: 1
   },
   {
-    src: "https://source.unsplash.com/qDkso9nvCg0/600x799",
+    src: "https://media.graphcms.com/8TePeM6ERXZI6bMNtiK1",
     width: 3,
     height: 4
   },
   {
-    src: "https://source.unsplash.com/iecJiKe_RNg/600x799",
+    src: "https://media.graphcms.com/Z4kdKLRMeyPb78l87SQA",
     width: 3,
     height: 4
   },
   {
-    src: "https://source.unsplash.com/epcsn8Ed8kY/600x799",
+    src: "https://media.graphcms.com/l2Cx60dwSdWitgzmFOBO",
     width: 3,
     height: 4
   },
   {
-    src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
+    src: "https://media.graphcms.com/kaLF8O1DQZeETZcDGM2n",
     width: 4,
     height: 3
   },
   {
-    src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
+    src: "https://media.graphcms.com/9BoLAURDCKFmHuuaniCQ",
     width: 3,
     height: 4
   },
   {
-    src: "https://source.unsplash.com/PpOHJezOalU/800x599",
+    src: "https://media.graphcms.com/HO4qzx1EQKya5iO9J7lu",
     width: 4,
     height: 3
   },
   {
-    src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
+    src: "https://media.graphcms.com/0j9OXRg3Stq60UbUgNc6",
     width: 4,
     height: 3
   },
   {
-    src: "https://source.unsplash.com/2ShvY8Lf6l0/800x599",
+    src: "https://media.graphcms.com/qwXBb7NR5e0YJZqiiZGO",
     width: 4,
     height: 3
   },
   {
-    src: "https://source.unsplash.com/Dm-qxdynoEc/800x799",
+    src: "https://media.graphcms.com/bxfaQCjTNC4aitvTOXf1",
     width: 1,
     height: 1
   },
   {
-    src: "https://source.unsplash.com/qDkso9nvCg0/600x799",
+    src: "https://media.graphcms.com/rbzIJYoVTBKLl5noBlfx",
     width: 3,
     height: 4
   },
   {
-    src: "https://source.unsplash.com/iecJiKe_RNg/600x799",
+    src: "https://media.graphcms.com/lQfubHuFRHqFBnifCZAC",
     width: 3,
     height: 4
   },
   {
-    src: "https://source.unsplash.com/epcsn8Ed8kY/600x799",
+    src: "https://media.graphcms.com/8UF9Yg3pRxqcL7DNFz45",
     width: 3,
     height: 4
   },
   {
-    src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
+    src: "https://media.graphcms.com/SeL3SEdjTGqTAoK8zcQ0",
     width: 4,
     height: 3
   },
   {
-    src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
+    src: "https://media.graphcms.com/LcEuIjsLQSaqBfD5j0cm",
     width: 3,
     height: 4
   },
-  {
-    src: "https://source.unsplash.com/PpOHJezOalU/800x599",
-    width: 4,
-    height: 3
-  },
-  {
-    src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
-    width: 4,
-    height: 3
-  }
 ];
