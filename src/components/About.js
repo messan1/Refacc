0c9157e112f4d559012a4d365eddd1459d54d58f
Messan christian <messanchristian@hotmail.com>
2019-08-05 18:07:44 +0000

    Partners added 5

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import aboutimg from '../images/aboutimg.png';
+import aboutimg from '../images/galeries/3.jpg';
 import {LazyLoadImage} from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 
